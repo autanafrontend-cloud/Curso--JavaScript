7eadd4798547f2a32c04a572c21c78c4c06740be
autanaweb <auatafrontend@gmail.com>
2026-04-13 20:06:13 +0200

    primeros ejercicios

---
+++ Exercise_1.js
@@ -0,0 +1,66 @@
+//Steps are to be completed in the app.js file.
+//1.	Declare a variable named firstName using the const keyword
+// 2.	Declare a variable named lastName using the let keyword, and assign your name as the string value
+// 3.	Declare a variable named age using the var keyword, and assign your age as the number value 
+
+// Exercise 1: Declaring Variables
+const firstName = "Ernesto";
+let lastName = "Vera";
+var age = 49;
+
+// Steps are to be completed in the app.js file.
+// 1.	Assign your first name as the string value to the variable firstName
+// 2.	Assign your last name as the string value to the variable lastName
+// 3.	Assign your age as the number value to the variable age
+
+
+// Exercise 2: Assigning Values to Variables
+// (ya están asignadas arriba, pero si quieres separarlo sería así:)
+let firstName2;
+let lastName2;
+var age2;
+
+firstName2 = "Ernesto";
+lastName2 = "Vera";
+age2 = 49;
+
+// Steps are to be completed in the app.js file.
+
+// Declare a variable named language using the let keyword, and assign it the value "JavaScript" (string)
+// Declare a variable named createdYear using the let keyword, and assign it the value 1995 (number)
+// Declare a variable named isCaseSensitive using the let keyword, and assign it the value true (boolean)
+// Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+// Steps are to be completed in the app.js file.
+
+// Declare a variable named price using the let keyword, and assign it the value 19.99 (number)
+// Declare a variable named isOnSale using the let keyword, and assign it the value false (boolean)
+// Declare a variable named salePercentage using the let keyword, and assign it the value 15 (number)
+// Declare a variable named stock using the let keyword, and assign it the value 0 (number)
+// Declare a variable named inStock using the let keyword, and assign it the value false (boolean)
+// Declare a variable named selectedSize using the let keyword, and assign it the value "M" (string)
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+// Steps are to be completed in the app.js file.
+// 1.	Declare a variable named title using the let keyword, and assign it the value "Name of the Wind" (string)
+// 2.	Declare a variable named author using the let keyword, and assign it the value "Patrick Rothfuss" (string)
+// 3.	Declare a variable named pageCount using the let keyword, and assign it the value 722 (number)
+// 4.	Declare a variable named bookmark using the let keyword, and assign it the value 456 (number)
+// 5.	Declare a variable named hasRead using the let keyword, and assign it the value true (boolean)
+
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
---
+++ exercise_2.js
@@ -0,0 +1,56 @@
+// Ejercicios Operadores:
+// 1.	Declare 2 variables, a and b, and assign 20 to a and 4 to b
+// 2.	Declare a variable add that uses the + operator to store the result of adding the values stored in a and b
+// 3.	Declare a variable minus that uses the - operator to store the result of subtracting the values stored in a and b
+// 4.	Declare a variable multiply that uses the * operator to store the result of multiplying the values stored in a and b
+// 5.	Declare a variable dividing that uses the / operator to store the result of dividing the values stored in a and b
+// You can print the value of the variables to the browser console (ex: console.log(add)) to check the result.
+
+
+const a = 20;
+const b = 4;
+
+const add = a + b;
+const minus = a - b;
+const multiply = a * b;
+const dividing = a / b;
+
+// Mostrar resultados en consola
+console.log("Suma:", add);
+console.log("Resta:", minus);
+console.log("Multiplicación:", multiply);
+console.log("División:", dividing);
+
+
+// Exercise 2#
+// 1.	Use the following code to answer the questions below:
+
+
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+console.log(num + str);
+console.log(num + str2);
+console.log(num + isPresent);
+console.log(firstName + num);
+console.log(isPresent + str);
+console.log(firstName + lastName);
+
+// Exercise 3#
+// 1.	Use the following code to answer the questions below:
+
+
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isAwake = false;
+
+console.log(val + str3);
+console.log(val + str4);
+console.log(val + isAwake);
+console.log(str3 + isAwake);
+console.log(str4 + val);
